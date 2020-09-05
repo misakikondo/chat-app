57cc1e84aa23a7d971dd84cb894d4e6c01e3b820
misakikondo <misakikondo718@gmail.com>
2020-09-05 22:30:31 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new()
+    @room = Room.find(params[:room_id])
+    #messageは新しいインスタンスに
+    #roomは選択したルームがサイドバーからparams[:room_id]として送信された
+
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+private
+ def message_params
+  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+ end 
 end
